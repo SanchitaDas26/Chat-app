bb59893675e3fc2ab231217b1d2a90829313401b
SanchitaDas26 <sanchitadas536@gmail.com>
2023-09-05 19:01:04 +0530

    third commit

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const path = require("path");
+
 
 
 const colors = require("colors");
@@ -23,6 +25,21 @@ app.use(express.json()); // to accept json data
 app.use("/api/user", userRoutes);
 app.use("/api/chat",chatRoutes);
 app.use("/api/message",messageRoutes);
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
 
 
 
